52e241766213d0d4e4aeaa41385c5b3cda436ab0
avinash-Kumar2004 <www.kumaravinash3898@gmail.com>
2026-02-03 16:40:29 +0530

    Fix wildcard route for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,10 +27,13 @@ app.use('/api/message', messageRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    app.get('/*', (req, res) => {
+        res.sendFile(
+            path.join(__dirname, '../frontend', 'dist', 'index.html')
+        );
     });
 }
+
 
 // Optional error handler
 app.use((err, req, res, next) => {
