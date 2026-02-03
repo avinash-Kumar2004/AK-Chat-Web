d44e4e0dc43fb2ecea90951d60a1fd0b129a4f46
avinash-Kumar2004 <www.kumaravinash3898@gmail.com>
2026-02-03 12:55:05 +0530

    again fix path

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,14 +29,10 @@ app.use('/api/message', messageRoutes)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  
-  app.get("/:path(*)", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend", "dist", "index.html")
-    );
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
 
 server.listen(PORT, () => {
   ConnectDB()
